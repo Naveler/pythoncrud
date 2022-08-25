2c897cb023ff17a48b09e76ab4c1a1a440e32900
Naveler <65281037+Naveler@users.noreply.github.com>
2022-08-25 22:47:10 +0300

    Add files via upload

---
+++ PythonCrud/python-flask-mysql-crud/tables.py
@@ -0,0 +1,15 @@
+from flask_table import Table, Col, LinkCol
+ 
+class Results(Table):
+    car_id = Col('Id', show=False)
+    car_name = Col('Name')
+    car_year = Col('Year')
+    edit = LinkCol('Edit', 'edit_view', url_kwargs=dict(id='car_id'))
+    delete = LinkCol('Delete', 'delete_car', url_kwargs=dict(id='car_id'))
+
+class DriverResults(Table):
+    driver_id = Col('Id', show=False)
+    driver_name = Col('Name')
+    driver_lastname = Col('Lastname')
+    edit = LinkCol('Edit', 'edit_driver', url_kwargs=dict(id='drivers_id'))
+    delete = LinkCol('Delete', 'delete_driver', url_kwargs=dict(id='drivers_id'))
